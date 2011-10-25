613a2115ed021079b8aec59796bc81b0fea0e5d0
Matthew Hartley <mhartley@cantab.net>
2011-10-25 11:27:07 +0100

    Getting closer

--- lsdup.py
+++ lsdup.py
@@ -5,6 +5,38 @@ import hashlib
 import subprocess
 import pprint
 
+def build_direntry(dirpath, dirnames, filenames):
+	d = {}
+	d['dchildren'] = [os.path.join(dirpath, dn) for dn in dirnames]
+	d['fchildren'] = [{os.path.join(dirpath, fn): 0} for fn in filenames]
+	#print fcs
+	#for fc in fcs:
+	#	print {fc: 0}
+
+	return d
+
+def build_dirtree(rootpath):
+
+	dtree = {}
+	for dirpath, dirnames, filenames in os.walk(rootpath):
+		#files += [os.path.join(dirpath, f) for f in filenames]
+		dtree[dirpath] = build_direntry(dirpath, dirnames, filenames)
+
+	for d in dtree:
+		for dc in dtree[d]['dchildren']:
+			dtree[dc]['parent'] = d
+
+	return dtree
+
+def tag_file_in_tree(dtree, filename, tag):
+	for d in dtree:
+		for fc in dtree[d]['fchildren']:
+			#print "TESTING %s AGAINST %s" % (fc, filename)
+			if filename in fc:
+				#print "MATCH"
+				fc[filename] = tag
+
+
 def md5sum_from_filename(filename):
 		return subprocess.check_output(["md5", "-q", filename]).strip()
 
@@ -27,6 +59,16 @@ def find_duplicates(files, comparator):
 
 	return dups
 
+def find_dir_dups(dtree):
+	for d in dtree:
+		dup = 1
+		for fc in dtree[d]['fchildren']:
+			for fn in fc:
+				dup = dup * fc[fn]
+		if dup == 1:
+			print "Duplicate directory: %s" % d
+			
+
 def list_from_dups(dups):
 	l = []
 	for d in dups:
@@ -41,6 +83,11 @@ for dirpath, dirnames, filenames in os.walk(rootpath):
 	files += [os.path.join(dirpath, f) for f in filenames]
 print "...%d found" % len(files)
 
+
+print "Building directory tree..."
+dtree = build_dirtree(rootpath)
+print "...%d directories found" % len(dtree)
+
 print "Building candidate duplicates list..."
 kdups_bysize = find_duplicates(files, os.path.getsize)
 ldups_bysize = list_from_dups(kdups_bysize)
@@ -53,8 +100,38 @@ print "...%d found" % len(ldups_byhash)
 
 for h in kdups_byhash:
 	for d in kdups_byhash[h]['r']:
-		print "=%s" % d
-	print ""
+		tag_file_in_tree(dtree, d, h)
+
+for d in dtree:
+	s = ""
+	for fc in dtree[d]['fchildren']:
+		for fn in fc:
+			if fc[fn]:
+				s += fc[fn]
+	#if s:
+		#print "%d, %d" % (len(s), len(dtree[d]['fchildren']) * 32)
+	if len(s) == len(dtree[d]['fchildren']) * 32:
+		dtree[d]['hash'] = hashlib.md5(s).hexdigest()	
+		print d, s, hashlib.md5(s).hexdigest()
+
+for d in dtree:
+	if dtree[d].has_key('hash'):
+		#print d, dtree[d]['hash']
+		pass
+
+#pprint.pprint(dtree)
+
+#for h in kdups_byhash:
+#	for d in kdups_byhash[h]['r']:
+#		print "=%s" % d
+#	print ""
+
+#for f in ldups_byhash:
+	#tag_file_in_tree(dtree, f)
+
+#find_dir_dups(dtree)
+
+#pprint.pprint(dtree)
 
 
 #md5sums = {}
